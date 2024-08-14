4a1bc4806609c187e115b7225daf40eb003a0e73
Thiniraxz <thiniragenuka@gmail.com>
2024-08-14 23:07:03 +0530

    Modify changes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,14 +3,15 @@ import { ERRORS } from '../common/constants';
 import { cbError } from '../common/handler';
 import { HTTPSTATUS } from '../common/enums';
 import AuthRoutes from './auth.routes';
+import { ROUTES } from '../common/constants';
 // import AirMobilityRoutes from './airMobility.routes';
 import { AppConfig } from '../config';
 
 export async function SetRoutes(app: Application) {
   const router = Router();
 
-  app.use(`${AppConfig.GEN_API}/${AppConfig.GEN_API_VERSION}`, router);
-  // app.use(`${AppConfig.UAS_API}/${AppConfig.UAS_API_VERSION}`, router);
+  app.use(`${ROUTES.GEN_API}/${AppConfig.GEN_API_VERSION}`, router);
+  // app.use(`${ROUTES.UAS_API}/${AppConfig.UAS_API_VERSION}`, router);
 
   new AuthRoutes(router);
   // new AirMobilityRoutes(router);
